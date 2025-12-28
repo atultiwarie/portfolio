f67f0670c14883ea5fce3a4857eea22a8fa38053
atultiwarie <atultiwari4767@gmail.com>
2025-12-28 09:55:25 +0530

    upadted resume and links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,7 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col md:flex-row items-center justify-around dark:bg-gray-800 dark:text-white p-6 mt-16 md:mt-24 transition-colors duration-300">
-  
+    <div className="min-h-screen flex flex-col md:flex-row items-center justify-around dark:bg-gray-800 dark:text-white p-6  md:mt-24 transition-colors duration-300">
       <div className="md:max-w-md text-center md:text-left">
         <h1 className="text-4xl sm:text-5xl font-bold mb-4">
           Hi, I’m <br />
@@ -14,22 +13,20 @@ const Home = () => {
           I build scalable and responsive web applications with the MERN stack.
         </p>
 
-     
         <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start">
           <a
-            href="https://drive.google.com/file/d/1_WxpQ0bu--_sNQnXlTs8GeEBFheYpcw6/view?usp=sharing"
+            href="https://drive.google.com/file/d/1vNO5uq_-e05QtOeZU7NNyNiOIMZTs268/view?usp=sharing"
             target="_blank"
             className="px-6 py-2 bg-emerald-500 rounded-lg shadow-md hover:bg-emerald-600 text-white transition-colors duration-300"
           >
             View Resume
           </a>
 
-   
           <Link
-            to="contact" 
+            to="contact"
             smooth={true}
             duration={500}
-            offset={-70} 
+            offset={-70}
             className="px-6 py-2 bg-white text-indigo-600 rounded-lg shadow-md hover:bg-gray-200 dark:bg-gray-700 dark:text-indigo-400 dark:hover:bg-gray-600 transition-colors duration-300 cursor-pointer"
           >
             Contact Me
@@ -37,10 +34,9 @@ const Home = () => {
         </div>
       </div>
 
- 
-      <div className="mt-8 md:mt-0">
+      <div className="mt-5 md:mt-0">
         <img
-          src="https://i.pinimg.com/1200x/0d/98/b2/0d98b2916254548f2c79a57eb8768969.jpg"
+          src="https://i.pinimg.com/736x/9c/d0/c9/9cd0c9f29763b323971ccb2e88f356e9.jpg"
           alt="Atul Tiwari"
           className="w-48 sm:w-64 md:w-[500px] h-48 sm:h-80 md:h-[600px] rounded-full object-cover mx-auto transition-all duration-300"
         />
